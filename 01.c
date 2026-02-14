fef7fa18f99975e9d0d67bb8f715bc181fc0bbc3
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-02-14 16:39:00 +0530

    .

--- 01.c
+++ 01.c
@@ -1,33 +1,35 @@
-#include<stdio.h> 
-void main() 
-{ 
-float a,b,res; 
-char op; 
-printf("Enter arithmetic expression[operand1 operator operand2]:"); 
-scanf("%f%c%f",&a,&op,&b); 
-switch(op) 
-{ 
-case'+': 
-res=a+b; 
-printf("\nsum=%f",res); 
-break; 
-case'-': res=a-b; 
-printf("\nDifference=%f",res); 
-break; 
-case'*': 
-res=a*b; 
-printf("\nProduct=%f",res); 
-break; 
-case'/':  
-res=a/b; 
-printf("\nQuotient=%f",res); 
-break; 
-default: printf("Invalid operator!"); 
-break; 
-} 
-} 
+#include <stdio.h>
+void main()
+{
+    float a, b, res;
+    char op;
+    printf("Enter arithmetic expression[operand1 operator operand2]:");
+    scanf("%f%c%f", &a, &op, &b);
+    switch (op)
+    {
+    case '+':
+        res = a + b;
+        printf("\nsum=%f", res);
+        break;
+    case '-':
+        res = a - b;
+        printf("\nDifference=%f", res);
+        break;
+    case '*':
+        res = a * b;
+        printf("\nProduct=%f", res);
+        break;
+    case '/':
+        res = a / b;
+        printf("\nQuotient=%f", res);
+        break;
+    default:
+        printf("Invalid operator!");
+        break;
+    }
+}
 
-/* 
+/*
 ALGORITHM:
 Step 1: Start
 Step 2: Input operand1, operator and operand2.
